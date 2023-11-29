b46120dcc57e7214cd093b792ba029201af1c94b
MaxPredathor <max.gilli98@gmail.com>
2023-11-29 11:24:57 +0100

    disc creation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-
-  </div>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 my-div d-flex flex-nowrap">
+          <div class="disc" v-for="disc in myArray">
+            <div class="disc" v-for="item in myArray[0]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -34,5 +42,21 @@ import { store } from './assets/data/store'
 </script>
 
 <style lang="scss" scoped>
+.my-div{
+  width: 100%;
+  background-color: #a6a6a6;
+  border-radius: 2em;
+  margin: 40px 0;
+  padding: 20px;
+
+  .disc{
+    width: 75px;
+    height: 75px;
+    border-radius: 50%;
+    border: 5px solid grey;
+    background-color: lightgray;
+    margin: 5px;
+  }
+}
 
 </style>

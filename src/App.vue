f775c09d7e8ac2f352fb4971b3a765ce9730dc73
MaxPredathor <max.gilli98@gmail.com>
2023-11-30 20:20:18 +0100

    progress

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <main>
     <div class="container">
-      <div class="row">
-        <div class="col-12 my-div d-flex flex-nowrap">
-          <div class="disc" v-for="disc in myArray">
-            <div class="disc" v-for="item in myArray[0]"></div>
+      <div class="row justify-content-center">
+        <div class="col-12 my-div d-flex justify-content-center">
+          <div v-for="(rows, rowsIndex) in biArray" :key="rowsIndex">
+            <div class="disc" v-for="(cols, colsIndex) in rows" :key="colsIndex" @click="redBlueSwitch = !redBlueSwitch, insertDisc(rowsIndex, colsIndex)"
+             :class="{'disc-blue': cols === 1, 'disc-red': cols === 0 }" ></div>
           </div>
+        </div>
+        <div v-if="redBlueSwitch" class="d-flex justify-content-center align-items-center blue">
+          <p>Blue Turn </p>
+          <div class="disc-blue"></div>
+        </div>
+        <div class="d-flex justify-content-center align-items-center red" v-else>
+          <p>Red Turn </p>
+          <div class="disc-red"></div>
         </div>
       </div>
     </div>
@@ -19,20 +28,31 @@ import { store } from './assets/data/store'
     data(){
       return{
         store,
-        rows: 10,
-        cols: 8,
-        myArray: [],
+        rows: 8,
+        cols: 6,
+        biArray: [],
+        redBlueSwitch: true
       }
     },
     methods:{
       arrayCreation(){
         for(let i = 0; i < this.rows; i++){
-          this.myArray[i] = []
+          this.biArray[i] = []
           for(let j = 0; j < this.cols; j++){
-            this.myArray[i][j] = null
+            this.biArray[i][j] = null
           }
         }
-        console.log(this.myArray)
+        this.biArray[0][5] = 1
+        console.log(this.biArray)
+      },
+      insertDisc(x, y){
+        // if(this.biArray[disc][item] !== 1 || this.biArray[disc][item] !== 0){
+          if(this.redBlueSwitch){
+          this.biArray[x][y] = 1
+          } else {
+          this.biArray[x][y] = 0
+          }  
+        // }   
       }
     },
     created(){
@@ -43,13 +63,15 @@ import { store } from './assets/data/store'
 
 <style lang="scss" scoped>
 .my-div{
-  width: 100%;
+  width: 70%;
   background-color: #a6a6a6;
   border-radius: 2em;
   margin: 40px 0;
   padding: 20px;
 
-  .disc{
+  
+}
+.disc{
     width: 75px;
     height: 75px;
     border-radius: 50%;
@@ -57,6 +79,37 @@ import { store } from './assets/data/store'
     background-color: lightgray;
     margin: 5px;
   }
-}
+  .disc-blue{
+    width: 75px;
+    height: 75px;
+    border-radius: 50%;
+    border: 5px solid rgb(5, 5, 68);
+    background-color: rgb(14, 14, 140);
+    margin: 5px;
+  }
+  .disc-red{
+    width: 75px;
+    height: 75px;
+    border-radius: 50%;
+    border: 5px solid rgb(72, 4, 4);
+    background-color: rgb(175, 24, 24);
+    margin: 5px;
+  }
+  .blue{
+
+    p{
+      color: rgb(14, 14, 140);
+      font-weight: bold;
+      font-size: 30px;
+    }
+  }
+  .red{
+
+    p{
+      color: rgb(175, 24, 24);
+      font-weight: bold;
+      font-size: 30px;
+    }
+  }
 
 </style>

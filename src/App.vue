8c8293ac3a2da38ad77187cf88f39ff081e5e247
MaxPredathor <max.gilli98@gmail.com>
2023-12-09 12:46:08 +0100

    added game win condition and post game screen with play again functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <main>
-    <div class="container">
-      <h1 class="text-center fw-bold mt-5">
+    <div class="container position-relative">
+      <h1 class="text-center fw-bold mt-5" style="font-size: 3.5em;">
         <span style="color:rgb(14, 14, 140)">Conn</span><span style="color:rgb(175, 24, 24)">ect </span><span style="color: #0a0b0c;">4</span>
       </h1>
+      <div class="win" v-show="gameEnd">
+        <h1 class="text-success fw-bold p-2">Congratulatios!</h1>
+        <div v-if="gameWinner === 1">
+          <h1 class="text-center" style="color:rgb(14, 14, 140)">Blue Wins</h1>
+          <p class="moves">With {{blueCounter}} Moves</p>
+          <button class="btn my-btn" @click="reset">Play Again</button>
+        </div>
+        <div v-else>
+          <h1 class="text-center" style="color:rgb(175, 24, 24)">Red Wins</h1>
+          <p class="moves">With {{redCounter}} Moves</p>
+          <button class="btn my-btn" @click="reset">Play Again</button>
+        </div>
+      </div>
       <div class="row justify-content-center">
         <div class="col-12 my-div d-flex justify-content-center">
           <div v-for="(rows, rowsIndex) in biArray" :key="rowsIndex">
             <div class="disk" v-for="(cols, colsIndex) in rows" :key="colsIndex" @click="insertDisk(rowsIndex, colsIndex), gameWin()"
-             :class="{'disk-blue': cols === 1, 'disk-red': cols === 0 }" ></div>
+             :class="{'disk-blue': cols === 1, 'disk-red': cols === 0, 'disabled': gameEnd }" ></div>
           </div>
         </div>
       </div>  
@@ -37,6 +50,10 @@
         cols: 6,
         biArray: [],
         redBlueSwitch: true,
+        gameEnd: false,
+        gameWinner: null,
+        blueCounter: 0,
+        redCounter: 0,
       }
     },
     methods:{
@@ -76,8 +93,10 @@
         if(this.biArray[x][y] !== 1 && this.biArray[x][y] !== 0){
           if(this.redBlueSwitch){
             this.diskVerticalCheck(x, 1)
+            this.blueCounter++
           }else{
             this.diskVerticalCheck(x, 0)
+            this.redCounter++
           }
         }    
       },
@@ -86,14 +105,27 @@
           for(let j = 0; j <= this.cols - 4; j++){
             const disk = this.biArray[i][j]
             if(disk !== null && disk === this.biArray[i][j + 1] && disk === this.biArray[i][j + 2] && disk === this.biArray[i][j + 3]){
-            console.log('hai vinto')  
+            this.gameWinner = disk
+            this.endGame()  
             }
           }
         }
       },
+      endGame(){
+        this.gameEnd = true
+      },
+      reset(){
+        this.arrayCreation()
+        this.gameEnd = false
+        this.gameWinner = null
+        this.redCounter = 0
+        this.blueCounter = 0
+        this.redBlueSwitch = true
+      }
     },
     created(){
       this.arrayCreation()
+      
     },
   }
 </script>
@@ -117,45 +149,81 @@
     background-color: #0a0b0c;
     margin: 5px;
   }
-  .disk-blue{
-    width: 75px;
-    height: 75px;
-    border-radius: 50%;
-    border: 5px solid rgb(5, 5, 68);
-    background-color: rgb(14, 14, 140);
-    margin: 5px;
-  }
-  .disk-red{
-    width: 75px;
-    height: 75px;
-    border-radius: 50%;
-    border: 5px solid rgb(72, 4, 4);
-    background-color: rgb(175, 24, 24);
-    margin: 5px;
-  }
-  .blue{
+.disk-blue{
+  width: 75px;
+  height: 75px;
+  border-radius: 50%;
+  border: 5px solid rgb(5, 5, 68);
+  background-color: rgb(14, 14, 140);
+  margin: 5px;
+}
+.disk-red{
+  width: 75px;
+  height: 75px;
+  border-radius: 50%;
+  border: 5px solid rgb(72, 4, 4);
+  background-color: rgb(175, 24, 24);
+  margin: 5px;
+}
+.blue{
 
-    p{
-      color: rgb(14, 14, 140);
-      font-weight: bold;
-      font-size: 30px;
-    }
+  p{
+    padding-top: 5px;
+    color: rgb(14, 14, 140);
+    font-weight: bold;
+    font-size: 30px;
   }
-  .red{
+}
+.red{
 
-    p{
-      color: rgb(175, 24, 24);
-      font-weight: bold;
-      font-size: 30px;
-    }
+  p{
+    padding-top: 5px;
+    color: rgb(175, 24, 24);
+    font-weight: bold;
+    font-size: 30px;
   }
-  .turnCard{
-    width: 22%;
-    background-color: #313131;
-    margin: 40px 0;
-    padding: 20px;
-    -webkit-box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0); 
-    box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0);
+}
+.turnCard{
+  width: 22%;
+  background-color: #313131;
+  margin: 40px 0;
+  padding: 20px;
+  -webkit-box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0); 
+  box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0);
+}
+.disabled{
+  pointer-events: none;
+}
+.win{
+  position: absolute;
+  top: 35%;
+  left: 40%;
+  background-color: #313131;
+  -webkit-box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0); 
+  box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0);
+  transition: all 1s ease;
+
+  div{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+
+  .moves{
+    color: #0a0b0c;
+    font-size: 1.5em;
+    text-align: center;
   }
+  .my-btn{
+    background-color: greenyellow;
+    color: #0a0b0c;
+    font-weight: bold;
+    font-size: 1.2em;
+    margin: 5px;
+
+  }
+  }
+  
+}
 
 </style>
